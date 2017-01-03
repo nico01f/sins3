c9e0f9349a6f419fb382c05737ad6613036baa2b
Nico Fuenzalida <nico01f@gmail.com>
2017-01-03 16:41:48 -0300

    basic app, layout and file upload

---
+++ app.rb
@@ -0,0 +1,44 @@
+require "rubygems"
+require 'sinatra'
+require "aws/s3"
+
+get '/' do
+  erb :index
+end
+
+# Sube objeto a cluster
+get '/fileUpload' do
+  erb :fileUpload
+end
+
+get '/listObject' do
+  erb :listObject
+end
+
+get '/pf' do
+  erb :fu
+end
+
+get '/status' do
+  erb :status
+end
+
+post '/upload' do
+  user_bucket     = 'NICO'
+  file       = params[:file][:tempfile]
+  filename   = params[:file][:filename]
+  AWS::S3::Base.establish_connection!(
+    :server            => 'ceph.autentia.cl',
+    :access_key_id     => 'ZYSIEXK5Q13052Y2D2EO',
+    :secret_access_key => 'DuEBOfP6AIItsnXYRvTDCRdGTV80ObIdMHrq6Mzm',
+    :bucket            => user_bucket
+  )
+  AWS::S3::S3Object.store(
+    filename,
+    open(file.path),
+    bucket,
+    :access => :public_read
+  )
+  url = "https://#{bucket}.ceph.autentia.cl/#{filename}"
+  return url
+end
